90b33a03ec9260fcdbe1625c990160cc117b6917
Shahed <atikshahed@gmail.com>
2024-06-28 15:49:21 +0400

    test: enhance the code

--- test/authentication/authentication_bloc_test.dart
+++ test/authentication/authentication_bloc_test.dart
@@ -1,19 +1,16 @@
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:task_manager/app/modules/app_bloc/app_bloc.dart';
 import 'package:task_manager/app/modules/app_bloc/app_event.dart';
 import 'package:task_manager/app/modules/app_bloc/app_state.dart';
-import 'package:task_manager/app/shared/http_client/refresh_token.dart';
 import 'package:task_manager/app/shared/models/user.dart';
-import 'package:task_manager/app/shared/services/local_storage_service.dart';
+import 'package:task_manager/app/shared/repos/auth_repo.dart';
 
-class _MockLocalStorageService extends Mock implements LocalStorageService {}
-
-class _MockTokenStorageImpl extends Mock implements TokenStorageImpl {}
+class _MockAuthRepo extends Mock implements AuthRepo {}
 
 void main() {
-  User user = User.fromMap({
+  User user = User.fromMap(const {
     "id": 1,
     "username": "emilys",
     "email": "emily.johnson@x.dummyjson.com",
@@ -26,13 +23,11 @@ void main() {
     "refreshToken":
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwidXNlcm5hbWUiOiJtaWNoYWVsdyIsImVtYWlsIjoibWljaGFlbC53aWxsaWFtc0B4LmR1bW15anNvbi5jb20iLCJmaXJzdE5hbWUiOiJNaWNoYWVsIiwibGFzdE5hbWUiOiJXaWxsaWFtcyIsImdlbmRlciI6Im1hbGUiLCJpbWFnZSI6Imh0dHBzOi8vZHVtbXlqc29uLmNvbS9pY29uL21pY2hhZWx3LzEyOCIsImlhdCI6MTcxNzYxMTc0MCwiZXhwIjoxNzIwMjAzNzQwfQ.YsStJdmdUjKOUlbXdqze0nEScCM_RJw9rnuy0RdSn88"
   });
-  late LocalStorageService localStorageService;
-  late TokenStorageImpl tokenStorageImpl;
+  late AuthRepo authRepo;
   setUp(() {
-    localStorageService = _MockLocalStorageService();
-    tokenStorageImpl = _MockTokenStorageImpl();
+    authRepo = _MockAuthRepo();
     when(
-      tokenStorageImpl.stream,
+      () => authRepo.tokenStorageStatus,
     ).thenAnswer((_) => const Stream.empty());
   });
 
@@ -40,13 +35,11 @@ void main() {
     blocTest<AppBloc, AppState>(
       'emits [unauthenticated] when user is not found',
       setUp: () {
-        when(localStorageService.getUser()).thenAnswer(
-          (_) => null,
+        when(() => authRepo.tokenStorageStatus).thenAnswer(
+          (_) => Stream.value(null),
         );
       },
-      build: () => AppBloc(
-          localStorageService: localStorageService,
-          tokenStorage: tokenStorageImpl),
+      build: () => AppBloc(authRepo: authRepo),
       expect: () => const <AppState>[
         AppState.unauthenticated(),
       ],
@@ -55,84 +48,46 @@ void main() {
     blocTest<AppBloc, AppState>(
       'emits [authenticated] when user is found',
       setUp: () {
-        when(localStorageService.getUser()).thenAnswer((_) => user);
+        when(() => authRepo.tokenStorageStatus).thenAnswer(
+          (_) => Stream.value(user),
+        );
+        when(() => authRepo.getLocalUser()).thenAnswer(
+          (_) => user,
+        );
       },
-      build: () => AppBloc(
-          localStorageService: localStorageService,
-          tokenStorage: tokenStorageImpl),
+      build: () => AppBloc(authRepo: authRepo),
       expect: () => <AppState>[
         AppState.authenticated(user),
-      ],
-    );
-
-    blocTest<AppBloc, AppState>(
-      'emits [unauthenticated] when the user is deleted',
-      setUp: () {
-        when(
-          tokenStorageImpl.stream,
-        ).thenAnswer((_) => Stream.value(null));
-        when(
-          localStorageService.getUser(),
-        ).thenAnswer((_) => null);
-      },
-      build: () => AppBloc(
-          localStorageService: localStorageService,
-          tokenStorage: tokenStorageImpl),
-      expect: () => <AppState>[
-        const AppState.unauthenticated(),
       ],
     );
   });
 
-  group('AppLoginRequested', () {
+  group('AppStatusChanged', () {
     blocTest<AppBloc, AppState>(
-      'calls setUser on localStorageService '
-      'when AppLoginRequested is added',
-      build: () => AppBloc(
-          localStorageService: localStorageService,
-          tokenStorage: tokenStorageImpl),
-      act: (bloc) => bloc.add(AppLoginRequested(user)),
-      verify: (_) {
-        verify(() => localStorageService.setUser(user)).called(1);
-      },
-    );
-
-    blocTest<AppBloc, AppState>(
-      'emits [authenticated] when save User success',
+      'emits [unauthenticated] when status is authenticated but getUser return null',
       setUp: () {
-        when(localStorageService.setUser(user)).thenAnswer((_) async => true);
+        when(
+          () => authRepo.tokenStorageStatus,
+        ).thenAnswer((_) => Stream.value(user));
+        when(() => authRepo.getLocalUser()).thenAnswer(
+          (_) => null,
+        );
       },
-      build: () => AppBloc(
-          localStorageService: localStorageService,
-          tokenStorage: tokenStorageImpl),
-      expect: () => <AppState>[
-        AppState.authenticated(user),
-      ],
-    );
-    blocTest<AppBloc, AppState>(
-      'emits [unauthenticated] when save User does not success',
-      setUp: () {
-        when(localStorageService.setUser(user)).thenAnswer((_) async => false);
-      },
-      build: () => AppBloc(
-          localStorageService: localStorageService,
-          tokenStorage: tokenStorageImpl),
-      expect: () => <AppState>[
-        const AppState.unauthenticated(),
+      build: () => AppBloc(authRepo: authRepo),
+      expect: () => const <AppState>[
+        AppState.unauthenticated(),
       ],
     );
   });
 
   group('AuthenticationLogoutRequested', () {
     blocTest<AppBloc, AppState>(
-      'calls deleteUserData on localStorageService '
+      'calls logOut on authRepo '
       'when AppLogoutRequested is added',
-      build: () => AppBloc(
-          localStorageService: localStorageService,
-          tokenStorage: tokenStorageImpl),
+      build: () => AppBloc(authRepo: authRepo),
       act: (bloc) => bloc.add(const AppLogoutRequested()),
       verify: (_) {
-        verify(() => localStorageService.deleteUserData()).called(1);
+        verify(() => authRepo.logOut()).called(1);
       },
     );
   });
